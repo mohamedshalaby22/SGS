5f55a091d9e146bc74732757234f1413a0e581e3
mohamedshalaby22 <shalabymohamed578@gmail.com>
2022-07-12 06:37:04 +0200

    Readme file update

--- lib/Screens/check_connection.dart
+++ lib/Screens/check_connection.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class CheckConnection extends StatelessWidget {
-  const CheckConnection({Key? key}) : super(key: key);
+  const CheckConnection({
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -13,36 +15,6 @@ class CheckConnection extends StatelessWidget {
             "assets/10_Connection Lost.png",
             fit: BoxFit.cover,
           ),
-          Positioned(
-            bottom: MediaQuery.of(context).size.height * 0.12,
-            left: MediaQuery.of(context).size.width * 0.065,
-            child: Container(
-                decoration: BoxDecoration(
-                  boxShadow: [
-                    BoxShadow(
-                      offset: const Offset(0, 5),
-                      blurRadius: 25,
-                      color: const Color(0xFF59618B).withOpacity(0.17),
-                    ),
-                  ],
-                ),
-                child: SizedBox(
-                  width: 150,
-                  height: 40,
-                  child: TextButton(
-                    style: TextButton.styleFrom(
-                        backgroundColor: Colors.blue.shade200,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(5),
-                        )),
-                    onPressed: () {},
-                    child: Text(
-                      "retry".toUpperCase(),
-                      style: const TextStyle(color: Colors.white),
-                    ),
-                  ),
-                )),
-          )
         ],
       ),
     );
--- lib/bottom_screens/home_layout.dart
+++ lib/bottom_screens/home_layout.dart
@@ -1,4 +1,6 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_3/Screens/check_connection.dart';
 import 'package:flutter_application_3/constant/const.dart';
 import 'package:flutter_application_3/controller/bottomnavb.dart';
 import 'package:get/get.dart';
@@ -8,23 +10,33 @@ class HomeLayOut extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetBuilder<BottomNavBar>(
-      init: BottomNavBar(),
-      builder: (value) => Scaffold(
-        backgroundColor: Colors.white,
-        bottomNavigationBar: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          showUnselectedLabels: true,
-          selectedItemColor: primaryColor,
-          unselectedItemColor: Colors.grey,
-          backgroundColor: Colors.white,
-          elevation: 20,
-          currentIndex: value.currentIndex,
-          onTap: (index) => value.chanagecurrentIndex(index),
-          items: value.bottomItem,
-        ),
-        body: value.screens[value.currentIndex],
-      ),
+    return StreamBuilder(
+      stream: Connectivity().onConnectivityChanged,
+      builder:
+          (BuildContext context, AsyncSnapshot<ConnectivityResult> snapshot) {
+        if (snapshot.hasData && snapshot.data != ConnectivityResult.none) {
+          return GetBuilder<BottomNavBar>(
+            init: BottomNavBar(),
+            builder: (value) => Scaffold(
+              backgroundColor: Colors.white,
+              bottomNavigationBar: BottomNavigationBar(
+                type: BottomNavigationBarType.fixed,
+                showUnselectedLabels: true,
+                selectedItemColor: primaryColor,
+                unselectedItemColor: Colors.grey,
+                backgroundColor: Colors.white,
+                elevation: 20,
+                currentIndex: value.currentIndex,
+                onTap: (index) => value.chanagecurrentIndex(index),
+                items: value.bottomItem,
+              ),
+              body: value.screens[value.currentIndex],
+            ),
+          );
+        } else {
+          return const CheckConnection();
+        }
+      },
     );
   }
 }
